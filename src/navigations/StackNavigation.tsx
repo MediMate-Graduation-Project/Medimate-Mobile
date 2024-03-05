a00316de61287f6f693c81fa41b1aefd8fc48205
dinhngocson0708 <ngocsondinh668@gmail.com>
2024-03-05 15:32:20 +0700

    Delete file voicedemo in screens folder

--- src/navigations/StackNavigation.tsx
+++ src/navigations/StackNavigation.tsx
@@ -6,15 +6,16 @@ import VoiceTest from "../screens/Symptom";
 import { Login } from "../screens/Login";
 import { Register } from "../screens/Register";
 import { VoiceDemo } from "../screens/voicedemo";
+import Symptom from "../screens/Symptom";
 
-// import AppDemo, { VoiceTest2 } from "../screens/Symptom_copy";
+
 
 export const StackNavigation=()=>{
     const Stack=createNativeStackNavigator();
     return(
         <Stack.Navigator>
             <Stack.Screen name="MainStack" component={MyDrawerNavigation} options={{headerShown:false}}></Stack.Screen>
-            <Stack.Screen name="voice" component={VoiceTest}></Stack.Screen>
+            <Stack.Screen name="voice" component={Symptom}></Stack.Screen>
             <Stack.Screen name="login" component={Login} options={{headerShown:false}}></Stack.Screen>
             <Stack.Screen name="register" component={Register} options={{headerShown:false}}></Stack.Screen>
         </Stack.Navigator>

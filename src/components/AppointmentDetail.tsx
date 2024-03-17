e0862b0847ec7ca6195ff5d4b2490376a4211483
dinhngocson0708 <ngocsondinh668@gmail.com>
2024-03-17 13:31:15 +0700

    Add module  Modal

--- src/components/AppointmentDetail.tsx
+++ src/components/AppointmentDetail.tsx
@@ -1,53 +1,61 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { ButtonItem } from "./ButtonItem";
-
+import Modal from "react-native-modal";
 export const AppointmentDetail = ({ prompt }: any) => {
 
     return (
-        <View style={styles.container}>
-            <View style={styles.containerTitle}><Text style={styles.textTitle}>THÔNG TIN ĐẶT LỊCH</Text></View>
-            <View style={styles.containerInfor}>
-                <Text style={styles.inforContainer}><Text>Họ và tên:</Text></Text>
-                <Text style={styles.inforContainer}><Text>Bệnh viện:</Text></Text>
-                <Text style={styles.inforContainer}><Text>Địa chỉ:</Text></Text>
-                <Text style={styles.inforContainer}><Text>Khoa khám:</Text></Text>
-                <Text style={styles.inforContainer}><Text>Phòng khám:</Text></Text>
-                <Text style={styles.inforContainer}><Text>Số thứ tự:</Text></Text>
-                <Text style={styles.inforContainer}><Text>Thời gian khám dự kiến:</Text></Text>
+        <Modal
+        isVisible={true}
+        animationIn={'bounceInLeft'}
+        >
+            <View style={styles.container}>
+                <View style={styles.containerTitle}><Text style={styles.textTitle}>THÔNG TIN ĐẶT LỊCH</Text></View>
+                <View style={styles.containerInfor}>
+                    <Text style={styles.inforContainer}><Text>Họ và tên: {prompt.name}</Text></Text>
+                    <Text style={styles.inforContainer}><Text>Bệnh viện: {prompt.hospital}</Text></Text>
+                    <Text style={styles.inforContainer}><Text>Địa chỉ: {prompt.address}</Text></Text>
+                    <Text style={styles.inforContainer}><Text>Khoa khám: {prompt.address}</Text></Text>
+                    <Text style={styles.inforContainer}><Text>Phòng khám: {prompt.address}</Text></Text>
+                    <Text style={styles.inforContainer}><Text>Số thứ tự: {prompt.address}</Text></Text>
+                    <Text style={styles.inforContainer}><Text>Thời gian khám dự kiến: {prompt.address}</Text></Text>
+                    
+                </View>
                 <ButtonItem
-                    handleOnpresLeft={null}
-                    titleLeft='Quay lại'
-                    handleOnpresRight={null}
-                    titleRight='Xác nhận đặt lịch' />
+                        handleOnpresLeft={null}
+                        titleLeft='Quay lại'
+                        handleOnpresRight={null}
+                        titleRight='Xác nhận đặt lịch' />
             </View>
+        </Modal>
 
-        </View>
     )
 }
 const styles = StyleSheet.create({
     containerTitle: {
         alignItems: 'center',
-        
+
     },
     container: {
-        flex: 1,
-        alignItems:'center',
-       
+        padding:20,
+        alignItems: 'center',
+        backgroundColor:'white',
+        borderRadius:15
     },
     textTitle: {
         fontWeight: 'bold',
-        fontSize:15,
-        marginBottom:10
+        fontSize: 15,
+        marginBottom: 10
     },
     containerInfor: {
         borderWidth: 1,
-        width:'auto',
-        height:'auto',
-        padding:10,
-        borderRadius:20
+        width:350,
+        height: 'auto',
+        padding: 10,
+        borderRadius: 20,
+        marginBottom:10
     },
-    inforContainer:{
-        padding:10
+    inforContainer: {
+        padding: 10
     }
 })
